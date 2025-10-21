de5b6b0e2cfcac2bdca4301b4d2137e61e342ac2
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-21 23:29:17 +0600

    day2 is done

--- day2.py
+++ day2.py
@@ -4,7 +4,10 @@ print("Hey! I'm a student \n" \
 "and a web developer") # \n is for new line
 
 '''
-This is a multi line comment using '''''' 6 quates
+This is a multi line comment using '''''' 6 quates and double quates """"""
 this is just for mulit line comment
 and this is blah blah blah
 '''
+print("Yes, this boy is \"crazy\"") # I used here escape character \ to display quates
+print("Hello world", 1, 2, 3, 4, 5) # I can print multiple values in print
+print("Hello world", 1, 2, 3, 4, 5, sep="~") # sep it separates by ~
