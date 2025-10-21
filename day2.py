afffd2cdafcf63ff7c51b47efab11ab056754bc7
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-21 22:40:45 +0600

    day2 is working

---
+++ day2.py
@@ -0,0 +1,10 @@
+# Now I will practice python Comments and Escape sequence 
+print("hello world")
+print("Hey! I'm a student \n" \
+"and a web developer") # \n is for new line
+
+'''
+This is a multi line comment using '''''' 6 quates
+this is just for mulit line comment
+and this is blah blah blah
+'''
