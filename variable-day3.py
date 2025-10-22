6cbff95cab545c07c34c48c82ab872fd863091e1
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-22 22:39:50 +0600

    ldsfldsk

---
+++ variable-day3.py
@@ -0,0 +1,9 @@
+# This chapter we will discuss about python variable
+# A variable is a container to store data
+a = 5
+b = 6
+c = "Kawsar"
+print(a)
+print(b)
+print(c)
+# Here a,b, and c are variable and value 5,6,"kawsar"
