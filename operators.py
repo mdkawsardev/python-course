6c1ad8e6c2a382dceccdf5809ac864c13564e332
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-22 23:37:13 +0600

    dfld

---
+++ operators.py
@@ -0,0 +1,32 @@
+# In this chapter we will discuss about python operators
+# Assignment operators
+# Assigning values in a variable
+a = 23
+b = 23
+c = 20
+b += a # b = b + a this is the defination
+b -= a # b = b - a this is the definition
+b *= a # b = b * a this is the defination
+b /= a # b = b / a this is the defination
+b %= a # b = b % a this is the defination
+c *= 2
+print(b)
+print(c)
+
+# Arithmetic operators
+x = 20 + 2
+y = 20 - 2
+z = 20 * 20
+xx = 20 / 20
+yy = 20 % 20
+print(x)
+
+# Comparison operators
+aa = 5 > 3
+bb = 2 < 4
+cc = 5 >= 5
+dd = 5 <= 5
+ee = 5 == 5
+ff = 5 != 5
+j = True
+print(type(j)) # to check dataType
--- variable-day3.py
+++ variable-day3.py
@@ -1,9 +1,31 @@
 # This chapter we will discuss about python variable
 # A variable is a container to store data
+# In python programming variable is case-sensitive, (age, Age, and AGE) different type of variable
+# A variable name can start with alpha-numeric and underscore
+# A variable cannot start with a number
 a = 5
 b = 6
 c = "Kawsar"
+d = 'imran'
 print(a)
 print(b)
 print(c)
 # Here a,b, and c are variable and value 5,6,"kawsar"
+# Python allows you to assign values to multiple variables in one line:
+x, y, z = "Imran", "kawsar", "emon"
+print(y)
+print(y)
+print(z)
+
+# One value to multiple variables
+ami=tumi=she = "kabo"
+print(she)
+
+# Unpacking variables
+fruits = ['apple', 'banana', 'orange']
+apple, banana, orange = fruits
+print(banana)
+py = "Python"
+Is = "is"
+awe = "awesome"
+print(py, Is, awe)
