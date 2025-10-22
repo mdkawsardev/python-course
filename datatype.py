ddbf4ba6f41af8b04d7c2b22e0bd5cea2e6e50b8
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-22 22:47:44 +0600

    dsfksdlf

---
+++ datatype.py
@@ -0,0 +1,6 @@
+# In this chapter we will discuss about python datatype
+a = 1 # this is a integer
+b = 1.3232 # this is a floating point number
+c = "Kawsar" # this is a string
+d = False # this is a boolean
+e = None # this is none
