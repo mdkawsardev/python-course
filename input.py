92884ff63cb7c5be2a256f2bfef4f6f5d9b038e1
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-23 23:54:00 +0600

    done

---
+++ input.py
@@ -0,0 +1,6 @@
+# a = int(input("Enter a number 1: "))
+# b = int(input("Enter a number 2: "))
+# print("The average number of a and b is", (a+b)/2) # to get average number of a and b
+x = int(input("Enter your number:"))
+print("The squre of this number is: ", x**2) # Both are same
+print("The squre of this number is: ", x*x) 
