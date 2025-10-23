a1433746a937647edf5a9526363a8adc3fab3e27
mdkawsar <info.mdkawsardev@gmail.com>
2025-10-23 23:17:02 +0600

    done

--- string.py
+++ string.py
@@ -29,3 +29,23 @@ price = "T-shirt {price: 0}"
 print(price.format(price = 400))
 myself = "my name is {fname}, I'm {age} years old"
 print(myself.format(fname = "kawsar", age = 23))
+dictionary = {"name": "Kawsar", "age": 23}
+txt = "Happy birthday {name} you're now level of {age}"
+print(txt.format_map(dictionary))
+print(name.index('h'))
+
+# f-string and format both are same
+# Using format mathod. Here .2f used for two float 00
+text1 = "I'm kawsar and I'm {age} years old. I'm in {level:.2f}".format(age = 23, level = 12)
+print(text1)
+# Using f-string
+identity = 'Web developer'
+iden = "Developer"
+text2 = f"I'm kawsar and I'm a {identity} and a python {iden}"
+print(text2)
+math = f"""
+math 1 = {2 + 2}
+math 2 = {3 + 3}
+math 3 = {4 + 4}
+"""
+print(math)
